8421ca7f51f1caa186ee903bf7d1aea3e0e75179
Lambda_School_Loaner_145 <lambda_school_loaner_145@Lambda-School-Loaner-145s-MacBook-Pro.local>
2019-08-22 11:43:12 -0700

    basic form built

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { withFormik, Form, Field } from "formik";
+
+// Needed: {Name email: "", password: ""} checkbox and submit button
 
 const Form() {
     return (
-
+        <div className = "loginForm" >{ console.log(user) }
+            <Form onSubmit = { event => handleSubmit(event) } >
+                <Label>Username:
+                    <input type = "text"
+                        name = "username"
+                        onChange = { event => handleChange(event) }
+                    /> 
+                </Label> 
+                <Label>Password:
+                    <input type = "text"
+                    name = "password"
+                    onChange = { event => handleChange(event) }
+                    />
+                </Label>
+                <button> Submit! </button> 
+            </Form> 
+        </div>
+    );
     );
 };
 
