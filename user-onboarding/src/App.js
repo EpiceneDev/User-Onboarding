98f982089f4f2341a327180cdc3dd54555d85990
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-09-19 12:37:55 -0700

    form.js built

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import UserForm from './components/Form.js';
+import Form from './components/Form.js';
 
 function App() {
     return ( 
         <div className = "App" >
-           
+           <Form />
         </div>
     );
 }
---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Form = () => (
+    <Form>
+        <label>
+            <input />
+        </label>
+    </Form>
+)
+
+export default Form;
